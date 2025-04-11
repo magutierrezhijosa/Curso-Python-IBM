2d817d0591a23dbb20d5f2ae13a104784c02894d
Soy Miguel <magutierrezhijosa@gmail.com>
2025-04-11 11:33:18 +0200

    servicio_snacks.py creado y funcionando

--- proyecto-manejo-de-archivos/maquina_snacks_proyecto/servicio_snacks.py
+++ proyecto-manejo-de-archivos/maquina_snacks_proyecto/servicio_snacks.py
@@ -1,4 +1,6 @@
 import os.path
+from snack import Snack
+
 
 class ServicioSnacks:
     NOMBRE_ARCHIVO = "snacks.txt"
@@ -14,8 +16,22 @@ class ServicioSnacks:
             self.cargar_snack_iniciales()
 
     def cargar_snacks_iniciales(self):
-        snacks_iniciales = [] 
+        snacks_iniciales = [
+            Snack("Papas", 70),
+            Snack("Refresco", 50),
+            Snack("Sandwich", 120)
+        ] 
 
+        self.snacks.extend(snacks_iniciales)
+        self.guardar_snacks_archivo(snacks_iniciales)
+
+    def guardar_snacks_archivo(self, snacks):
+        try:
+            with open(self.NOMBRE_ARCHIVO, "a") as archivo:
+                for snack in snacks:     
+                    archivo.write(f"{snack.escribir_snack()}\n")
+        except Exception as e:
+            print(f"Error al guardar snacks en archivo: {e}")    
     # def cargar_snacks_inicia:
     def obtener_snacks(self):
         pass
