65bd47c2393a1eefd735274c59a23caaafe22075
Soy Miguel <magutierrezhijosa@gmail.com>
2025-04-18 18:49:36 +0200

    Nuevo Proyecto sistema_de_gestion_de_contactos

---
+++ sistema_de_gestion_de_contactos/contacto.py
@@ -0,0 +1,12 @@
+
+
+
+class Contacto:
+
+    def __init__(self,nombre,numero_telefono,correo):
+        self.nombre = nombre
+        self.numero_telefono = numero_telefono
+        self.correo = correo
+
+    def __str__(self):
+        pass
