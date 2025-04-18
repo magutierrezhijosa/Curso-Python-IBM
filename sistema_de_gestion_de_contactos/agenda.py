286ce95bc5d2f3e854b122a1e99d838bb9d13596
Soy Miguel <magutierrezhijosa@gmail.com>
2025-04-18 20:29:51 +0200

    Metodo menu_agenda creado

--- sistema_de_gestion_de_contactos/agenda.py
+++ sistema_de_gestion_de_contactos/agenda.py
@@ -15,3 +15,24 @@ class Agenda:
         # Declaro la variable par acontrolar el bucle while
         salir = False
         print("*** Bienvenido a la agenda ***")
+
+    # Creo un Menu para mostrar las opciones que podemos hacer con nuestra agenda
+    def menu_agenda():
+        print(f"""--- MENU DE PELICULAS ---
+              1. Agregar contacto  
+              2. Mostrar contactos
+              3. Buscar contacto
+              4. Eliminar contacto
+              5. Salir
+               """)
+        opcion = input("Elige una opcion: ")
+
+        # Comprobamos que el valor introducido sea un numero
+        # de lo contrario mandamos un mensaje de error 
+        if opcion.isdigit():
+            return int(opcion)
+        else:
+            print("Por favor, introduce un numero valido.")
+            
+    if __name__ == "__main__":
+        pass
