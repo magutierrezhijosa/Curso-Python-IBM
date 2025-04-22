743f22659de7ae1ab579e9fbcde8ec9ed6ba0abe
Soy Miguel <magutierrezhijosa@gmail.com>
2025-04-23 00:17:21 +0200

    Comentario anexar

--- proyecto-manejo-de-archivos/anexar_archivo.py
+++ proyecto-manejo-de-archivos/anexar_archivo.py
@@ -2,6 +2,7 @@ print("*** Anexar  informacion Archivo ***")
 
 nombre_archivo = "mi_archivo.txt"
 
+# Abrimos el archivo para escribirlo 
 with open(nombre_archivo, "a") as archivo:
     # Anexar informacion al archivo
     archivo.write("Anexando informacion ...  \n")
