1856bcd8b5ad6aadd257f901de7f91d139e3f2b9
Soy Miguel <magutierrezhijosa@gmail.com>
2025-04-23 12:50:48 +0200

    actualizar_personas Funcionando correctamente

--- PersonasDB/actualizar_persona.py
+++ PersonasDB/actualizar_persona.py
@@ -14,3 +14,18 @@ personas_db = mysql.connector.connect(
 
 # Agregamos le objeto cursor() que no nos permite enviar y recibir consultas SQL
 cursor = personas_db.cursor()
+# Creamos la sentencia tipo UPDATE para actualizar un campo 
+sentencia_sql = "UPDATE personas SET nombre=%s, apellido=%s, edad=%s WHERE id=%s"
+# Guardamos en una variable los valores que vamos a actualizar en la DB
+valores = ("Victoria", "Flores", 45, 5) 
+# Llamamos al metodo execute() para ejecutar nuestra sentencia y agregar los valores que tenemos ya guardados en la variable 
+cursor.execute(sentencia_sql,valores)
+# Guardamos los cambios en la base de datos
+personas_db.commit()
+# Mostramos un mensage en pantalla que nos indique que se actualizaron los campos
+print(f"Se ha modificado la informacion a : {valores}")
+# Cerramos el cursor
+cursor.close()
+# Cerramos la DB 
+personas_db.close()
+
