56f75faf18a91e787b1d2db5c65f1fb3d382d5b1
Soy Miguel <magutierrezhijosa@gmail.com>
2025-04-23 12:08:53 +0200

    Conxion a MySQL realizada con comentarios en el codigo

--- PersonasDB/select_personas.py
+++ PersonasDB/select_personas.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+# Creamos la conexion a MySQL 
 personas_db = mysql.connector.connect(
 
     host ="localhost",
@@ -10,14 +11,19 @@ personas_db = mysql.connector.connect(
 
 )
 
-
+# Agregamos le objeto cursor() que no nos permite enviar y recibir consultas SQL
 cursor = personas_db.cursor()
+# En esta linea ejecutamnos la consulta que queramos hacer a nuestra DB 
 cursor.execute("SELECT * FROM personas")
+# Recogemos el resultado de la consulta y lo guardamos en una variable 
 resultado = cursor.fetchall()
 
+# Recorremos las lineas del resultado iterando con un for
 for linea in resultado:
-
+    # Mostramos en pantalla los resultados
     print(linea)
 
+# Tras realizar nuestras operaciones cerramos el cursor 
 cursor.close()
+# Cerramos la base de datos
 personas_db.close()
