50558e442911e62a02156b5335aff3f93b2256c7
Soy Miguel <magutierrezhijosa@gmail.com>
2025-04-30 13:18:00 +0200

    Declaradas las constantes para guardar las Querys

--- sistema_gestion_inventario/cliente_dao.py
+++ sistema_gestion_inventario/cliente_dao.py
@@ -4,5 +4,16 @@
 class ClienteDAO:
 
     # Esta clase va a tener la funcionalidad para poder interactuar con objetos de tipo producto y realizar CRUD con los datos
+
     # Declaramos las constantes de clase par ahacer posteriormente consultas con ellas
     SELECCIONAR = "SELECT * FROM producto ORDER BY id"
+    INSERTAR = "INSERT INTO producto(nombre, cantidad, precio, categoria) VALUES (%s, %s, %s)"
+    ACTUALIZAR = "UPDATE producto SET nombre=%s, cantidad=%s, precio=%s, categoria=%s"
+    ELIMINAR = "DELETE FROM producto WHERE id=%s"
+
+    # Vamos a crear los metodos de clase para realizar las consultas a la base de datos
+    @classmethod
+    def seleccionar(cls):
+
+        # Declaramos la variable para guardar el estado de la conexion
+        conexion = None
