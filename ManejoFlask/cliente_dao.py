3b5488dae469fb5394d98693ef7a539c11aa62ab
Soy Miguel <magutierrezhijosa@gmail.com>
2025-05-06 13:42:34 +0200

    Importacion de la clases de Zona Fit para poder realizar la conexion a la DB falta editar su contenido

---
+++ ManejoFlask/cliente_dao.py
@@ -0,0 +1,162 @@
+
+# Creamos la clase ClienteDAO
+from conexion import Conexion
+from cliente import Cliente
+
+
+class ClienteDAO:
+    # Esta clase va tener la funcionalidad para poder interactuar con objeto
+    # de tipo cliente y realizar CRUD con los datos
+    SELECCIONAR = "SELECT * FROM cliente ORDER BY id"
+    INSERTAR = "INSERT INTO cliente(nombre, apellido, membresia) VALUES(%s, %s, %s)"
+    ACTUALIZAR = "UPDATE cliente SET nombre=%s, apellido=%s, membresia=%s WHERE id=%s"
+    ELIMINAR = "DELETE FROM cliente WHERE id=%s"
+
+    @classmethod
+    # Trabajamos con metodos de clase
+    def seleccionar(cls):
+        conexion = None
+
+        try:
+            # Vamos a pedir una conexion a la clase Conexion
+            conexion = Conexion.obtener_conexion()
+            # Declaramos la variable de cursor
+            cursor = conexion.cursor()
+            # Ejecutamos la QUERY que tenemos almacenada el la CONSTANTE de clase
+            # por medio del cursor que hemos creado anteriormente
+            cursor.execute(cls.SELECCIONAR)
+            # Guardamos todos los registros de la QUERY que hicimos al Cliente
+            registros = cursor.fetchall()
+            # Mapeo de clase-tabla cliente
+            clientes =[]
+            for registro in registros:
+                # Guardamos cada registro en una variable cliente
+                cliente = Cliente(registro[0], registro[1], registro[2], registro[3])
+
+                # Agregamos nuestro Objeto de tipo Cliente a nuesta lista de clientes[]
+                clientes.append(cliente)
+            # Por ultimo devolvemos la list ade Objetos de tipo Cliente
+            return clientes
+
+        except Exception as e:
+            print(f"Ocurrio un error al seleecionar clientes: {e}")
+        #
+        finally:
+            # Revisamos el objeto conexion para cerrarlo en el caso de que no sea None
+            if conexion is not None:
+                # Cerramos el cursor
+                cursor.close()
+                Conexion.liberar_conexion(conexion)
+
+    # Creamos otro metodo de clase
+    @classmethod
+    def  insertar(cls, cliente):
+        # Definimos la variable de conexion
+        conexion = None
+
+        try:
+            # Creamos nuestra conexion llamando a la clase COnexion
+            conexion = Conexion.obtener_conexion()
+            # Creamos nuestro objeto cursor  usando la variable conexion
+            cursor = conexion.cursor()
+            # Agregamos los valores de los parametros segun declaramos en la constante INSERTAR
+            valores =  (cliente.nombre, cliente.apellido, cliente.membresia)
+            print(valores)
+            # Usando el cursor llamamos al metodo execute para ejecutar la consulta de tipo insert usadon la consstante INSERT
+            cursor.execute(cls.INSERTAR, valores)
+            # Guardamos los cambios en la base de datos con .commit()
+            conexion.commit()
+            # Podemos retornar de manera opciona cuantos valores se modificaron en la base de datos
+            return cursor.rowcount
+
+        except Exception as e:
+            print(f"Ocurrio un error al insertar: {e}")
+        finally:
+            # Revisamos el objeto conexion para cerrarlo en el caso de que no sea None
+            if conexion is not None:
+                # Cerramos el cursor
+                cursor.close()
+                Conexion.liberar_conexion(conexion)
+
+    @classmethod
+    def actualizar(cls,cliente):
+        # Definimos la variable de conexion
+        conexion = None
+        try:
+            # Creamos un objeto de tipo conexion
+            conexion = Conexion.obtener_conexion()
+            # Definimos la variable de cursor y a traves de la conexion creamos un objeto de tipo cursor
+            cursor = conexion.cursor()
+            # recogemos los valores que vamos a introducir como parametros mas adelante
+            valores = (cliente.nombre, cliente.apellido,
+                       cliente.membresia, cliente.id)
+            # mediante el cursor ejecutamos la QUERY de actualizar nuestra tabla junto con los valores que hemos guardado
+            cursor.execute(cls.ACTUALIZAR, valores)
+            # Finalmente guardamos los cambios con un commit
+            conexion.commit()
+            # Devolvemos cuantos registros se modificaron
+            return cursor.rowcount
+        except Exception as e:
+            print(f"Ocurrio un error al actualizar un cliente: {e}")
+        finally:
+            # Revisamos el objeto conexion para cerrarlo en el caso de que no sea None
+            if conexion is not None:
+                # Cerramos el cursor
+                cursor.close()
+                Conexion.liberar_conexion(conexion)
+
+    @classmethod
+    def eliminar(cls,cliente):
+        conexion = None
+
+        try:
+            # Creamos el objeto de tipo Conexion
+            conexion = Conexion.obtener_conexion()
+            # A partir de la conecion creamos el objeto de tipo cursor
+            cursor = conexion.cursor()
+            # Declaramos la variable con el valor de id
+            #***********IMPORTANTE*****************************
+            # PARA QQUE SEA UNA TUPLA DEBEMOS ESCRIBIR UNA COMA DESPUES DE EL VALOR SINO NO LA RECONOCE COMO TUPLA
+            #************************************************
+            valores = (cliente.id,)
+            # A partir del objeto cursor ejecutamos la QUERY que teniamos el la constante de clase
+            cursor.execute(cls.ELIMINAR, valores)
+            # Guardamos los cambios en la DB
+            conexion.commit()
+            # Devolvemos los registros que se han eliminado
+            return cursor.rowcount
+
+        except Exception as e:
+            print(f"Ocurrio un error al eliminar un cliente: {e}")
+        finally:
+            # Revisamos el objeto conexion para cerrarlo en el caso de que no sea None
+            if conexion is not None:
+                # Cerramos el cursor
+                cursor.close()
+                Conexion.liberar_conexion(conexion)
+
+# Vamos a realizar una prueba para comprobar que recibimos los objetos y ademas
+# se esten conviertiendo a objetos de tipo cliente
+if __name__ == "__main__":
+    # # Insertar cliente
+    # # Utilizamos el constructor de la clase cliente sin el id para que sea insertar ya que si le indicamos un id la consulta seria de update
+    # cliente1 = Cliente(nombre="Alejandra", apellido="Tellez", membresia=300)
+    # # Llamamos al metodo de la clase ClienteDAO para insertar
+    # clientes_insertados = ClienteDAO.insertar(cliente1)
+    # # Mostramos en pantalla los clientes que hemos insertado
+    # print(f"Clientes insertados: {clientes_insertados}")
+
+    # # Actualizar un cliente
+    # cliente_actualizar = Cliente(3, "Alexa", "Tellez", 400)
+    # clientes_actualizados = ClienteDAO.actualizar(cliente_actualizar)
+    # print(f"Clientes actualizados: {clientes_actualizados}")
+
+    # Eliminar cliente
+    cliente_eliminar = Cliente(id=3)
+    clientes_eliminados = ClienteDAO.eliminar(cliente_eliminar)
+    print(f"Se elimino el usuario: {clientes_eliminados}")
+
+    # Seleccionas lo cliente
+    clientes = ClienteDAO.seleccionar()
+    for cliente in clientes:
+        print(cliente)
---
+++ ManejoFlask/conexion.py
@@ -0,0 +1,65 @@
+from mysql.connector import pooling
+from mysql.connector import Error       
+
+# Creo la clase Conexion para tener un pool de objetos tipo conexion
+# y asi conseguir que los usuarios que se concectan puedan acceder 
+# mas rapido a la DATABASE 
+class Conexion:
+    DATABASE = "zona_fit_db"
+    USERNAME = "root"
+    PASSWORD = "admin"
+    DB_PORT = "3303"
+    HOST = "localhost"
+    POOL_SIZE = 5
+    POOL_NAME = "zona_fit_pool"
+    pool = None
+
+    # Creamos el metodo de clase 
+    @classmethod
+    # Definimos el motodo de clase con un parametro tipo ***cls*** que nos va a permitir acceder a todas las constantes de nuestra clase
+    def obtener_pool(cls):
+        # Si la variable clase pool es None 
+        if cls.pool is None: # Se crea el objeto pool
+            try:
+                cls.pool = pooling.MySQLConnectionPool(
+                    # Agregamos valores a cada una de las constantes que hemos ido definiendo
+                    pool_name = cls.POOL_NAME,
+                    pool_size = cls.POOL_SIZE,
+                    host = cls.HOST,
+                    port = cls.DB_PORT,
+                    database = cls.DATABASE,
+                    user = cls.USERNAME,
+                    password = cls.PASSWORD
+
+
+                )
+                return cls.pool
+            except Error as e:
+                print(f"Ocurrio un error al obtener pool: {e}")
+
+        # En el caso de que no sea None retornamos el objeto de tipo pool
+        else:
+            return cls.pool
+        
+    @classmethod
+    # Metodo para crear nuevos objetos de tipo conexion 
+    def obtener_conexion(cls):
+        return cls.obtener_pool().get_connection()
+    
+    @classmethod
+    # Metodo para cerrar la conexion una vez haya sido utilizada
+    def liberar_conexion(cls, conexion):
+        conexion.close()
+
+# Prueba par comprobar que todo funciona correctamente y esta bien importado
+if __name__ == "__main__":
+
+    # Creamos un objeto pool
+    pool = Conexion.obtener_pool()
+    print(pool)
+    # Creamos un objeto de tipo conexion 
+    conexion1 = pool.get_connection()
+    print(conexion1)
+    # Por ultimo cerramos la conexion 
+    Conexion.liberar_conexion(conexion1)
+    print(f"Se ha liberado el objeto conexion1")
