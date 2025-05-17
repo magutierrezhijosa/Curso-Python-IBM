6386e909309c876e36bef71c089a80f7cb08d309
Soy Miguel <magutierrezhijosa@gmail.com>
2025-05-17 12:48:05 +0200

    Creacion de vistas en views.py  de nuestro proyecto en Django

--- Modulo-Django/Django-Install-Curso/core/views.py
+++ Modulo-Django/Django-Install-Curso/core/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
+# Creamos una variable de tipo cadena 
+html_base = """
+<h1>Mi web personal</h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about-me/ ">Acerca de </a></li>
+    <li><a href="/">Portafolio</a></li>
+    <li><a href="/">Contacto</a></li>
+</ul>
+
+"""
 
 # Create your views here.
+# Creamos la vista para mostrar home
+def home(request):
+    # Devolvemos la respuesta de tipo HttpResponse
+    return HttpResponse(html_base + "<h2>Portada</h2><p>Esto es la portada </p>")
+
+#  Creamos una vista para mostrar el About
+def about(request):
+    return HttpResponse(html_base + "<h2>Acerca de </h2><p>Me llamo MichelAngelo y soy programador</p>")
+
+# Creamos la vista para mostrar el Portfolio
+def portfolio(request):
+
+    return HttpResponse(html_base + "<h2>Portafolio</h2><p>Este es el Portfolio de Michelangelo</p>")
+
+# Creamos la vista de Contacto
+def contacto(request):
+
+    return HttpResponse(html_base + "<h2>Contacto</h2><p>Contacto de Michelangelo</p>")
