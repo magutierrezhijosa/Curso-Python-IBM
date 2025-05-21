9b3e89138b9630e4d11416897667c9d300a9f469
Soy Miguel <magutierrezhijosa@gmail.com>
2025-05-21 19:46:52 +0200

    Creando modelo para crear modelo de Proyecto

--- Modulo-Django/Django-Install-Curso/portfolio/models.py
+++ Modulo-Django/Django-Install-Curso/portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# Esta clase va a representar una tabla 
+class Proyect(models.Model):
+    # Campos que vamos a usar  en nuestra tabla 
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
